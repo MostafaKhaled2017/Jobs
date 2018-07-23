5375676ade02f6102ad2b55584683e37778f3d4f
MostafaKhaled2017 <mostafakhaled835@outlook.com>
2018-07-24 01:18:36 +0200

    initial commit

--- app/src/main/java/com/mk/jobs/activities/MainActivity.java
+++ app/src/main/java/com/mk/jobs/activities/MainActivity.java
@@ -1,12 +1,14 @@
 package com.mk.jobs.activities;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
 
 import com.mk.jobs.R;
+import com.mk.jobs.adapters.ExpandableListAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -14,11 +16,69 @@ import java.util.List;
  */
 
 public class MainActivity extends AppCompatActivity{
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    ArrayList<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get the listview
+        expListView = findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        // Adding child data
+        listDataHeader.add("Doctors");
+        listDataHeader.add("Engineers");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> doctors = new ArrayList<>();
+        doctors.add("Allergists/Immunologists");
+        doctors.add("Anesthesiologists ");
+        doctors.add("Cardiologists");
+        doctors.add("Colon and Rectal Surgeons");
+        doctors.add("Critical Care Medicine Specialists");
+        doctors.add("Dermatologists");
+        doctors.add("Endocrinologists ");
+        doctors.add("others");
+
+        List<String> engineers = new ArrayList<>();
+        engineers.add("Computer Engineer");
+        engineers.add("Electrical Engineer");
+        engineers.add("Civil Engineer");
+        engineers.add("Mechanical Engineer");
+        engineers.add("Architectural Engineer");
+        engineers.add("others");
+
+        List<String> comingSoon = new ArrayList<>();
+        comingSoon.add("job 1");
+        comingSoon.add("job 2");
+        comingSoon.add("job 3");
+
+        listDataChild.put(listDataHeader.get(0), doctors); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), engineers);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 }
+
